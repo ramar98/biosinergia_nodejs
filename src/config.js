7b7a1e9f6c32dfbbf12830c7f9bb0a3876f3a194
Ramiro Gonzalez <ramiro.gonzalez@grupocarreras.com>
2025-04-15 21:22:41 -0300

    cambioDataBase

--- src/config.js
+++ src/config.js
@@ -4,6 +4,6 @@ config();
 export const PORT = process.env.PORT || 8080;
 export const DB_HOST = process.env.DB_HOST || "mysql-u9qm.railway.internal";
 export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "VCvtMNcvaAWRwIGHCgRfxhszjMuXICZu";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "ZSuwvGEuzChJfXwzEQAfBplGivlomUYe";
 export const DB_DATABASE = process.env.DB_DATABASE || "railway";
 export const DB_PORT = process.env.DB_PORT || 3306;
--- src/controllers/meditions.controller.js
+++ src/controllers/meditions.controller.js
@@ -5,7 +5,7 @@ export const getMeditions = async (req, res) => {
     const [rows] = await pool.query("SELECT * FROM medition");
     res.json(rows);
   } catch (error) {
-    return res.status(500).json({ message: "Something goes wrong" });
+    return res.status(500).json({ message: "sdsad" });
   }
 };
 
