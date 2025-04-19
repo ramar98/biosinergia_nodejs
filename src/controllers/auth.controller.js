605b21853a4c69b9c9a3182b92fa879d12266b08
Ramiro Gonzalez <ramiro.gonzalez@grupocarreras.com>
2025-04-19 16:50:15 -0300

    Register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,7 +4,7 @@ import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
     const { email, password } = req.body;
-    
+
     try {
         const [rows] = await pool.query(
             "SELECT * FROM user WHERE email = ?",
@@ -19,11 +19,11 @@ export const login = async (req, res) => {
         if (!match) {
             return res.status(401).json("Invalid credentials");
         }
-    
+
         if (rows.length === 0) {
-        return res.status(401).json("Invalid credentials");
+            return res.status(401).json("Invalid credentials");
         }
-    
+
         res.json(rows[0]);
     } catch (error) {
         console.error(error.message);
@@ -32,8 +32,8 @@ export const login = async (req, res) => {
 };
 
 export const register = async (req, res) => {
-    const { username, password, email} = req.body;
-    
+    const { username, password } = req.body;
+
     try {
         // Generate a salt to use for hashing
         const salt = await bcrypt.genSalt(10);
@@ -41,10 +41,10 @@ export const register = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const [rows] = await pool.query(
-            "INSERT INTO user (username, password, email) VALUES (?, ?, ?)",
-            [username, hashedPassword, email]
-          );
-          res.status(201).json({ id: rows.insertId, username, hashedPassword, email });
+            "INSERT INTO user (username, password) VALUES (?, ?)",
+            [username, hashedPassword]
+        );
+        res.status(201).json({ id: rows.insertId, username, hashedPassword });
     } catch (error) {
         console.error(error.message);
         res.status(500).json("Server error");
